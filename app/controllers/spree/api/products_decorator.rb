0fc4154eb45da9974393f8a32dc2d5643bfd7cd4
Jason Knebel <jasonkneb@gmail.com>
2015-04-17 14:21:56 -0400

    customizing products api

---
+++ app/controllers/spree/api/products_decorator.rb
@@ -0,0 +1,17 @@
+Spree::Api::ProductsController.class_eval do
+
+  private
+
+  def collection
+    return 'brand needed' unless params['brand'].present?
+    brand_id = Spree::Brand.find_by_title(params['brand']).id
+    products = Spree::Product.active.available.where(brand_id: brand_id)
+    @collection = { products: products.map { |p|
+      { sku: p.sku, name: p.name, permalink: p.permalink } } }
+  end
+
+  def object_serialization_options
+    # overiding method provided by SpreeApi
+  end
+
+end
