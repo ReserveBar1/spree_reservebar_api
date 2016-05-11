4a1e711d0e370a69974d5999024cd22602cbabfb
Jason Knebel <jasonkneb@gmail.com>
2016-05-11 15:06:04 -0400

    serving up all brands, even 'Not Set'

--- app/controllers/spree/api/brands_controller.rb
+++ app/controllers/spree/api/brands_controller.rb
@@ -6,7 +6,7 @@ class Spree::Api::BrandsController < Spree::Api::BaseController
   private
 
   def collection
-    brands = Spree::Brand.where('title != ?', 'Not Set').map { |b| { title: b.title, id: b.id } }
+    brands = Spree::Brand.all.map { |b| { title: b.title, id: b.id } }
     @collection = { brands: brands }
   end
 
